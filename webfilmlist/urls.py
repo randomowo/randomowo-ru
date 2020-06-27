c77ee033a3c035cb7c5c3e6192a56e08e13bede8
randomowo <randomowo@ya.ru>
2020-06-27 06:03:06 +0300

    [urls] * added pages urls (yesno, webms)

--- webfilmlist/urls.py
+++ webfilmlist/urls.py
@@ -18,10 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from bestmoments.views import webms
+from index.views import yesno
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("cinema/", include("index.urls")),
+    path("yesno/", yesno),
+    path("webms/", webms),
 #    path("cinema/admin", include("admin.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
