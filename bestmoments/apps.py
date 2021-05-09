9b24dc312d4f41a29abe1d661100d6aaa3bc6019
randomowo <randomowo@ya.ru>
2021-05-10 01:54:01 +0300

    deleted old version
    * updated gitignore

--- bestmoments/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BestmomentsConfig(AppConfig):
-    name = 'bestmoments'
--- filmlist/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilmlistConfig(AppConfig):
-    name = 'filmlist'
